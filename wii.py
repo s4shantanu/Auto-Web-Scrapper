ac07f0a611486f8834647a8096473f6d1dd3b6c1
Shanatnu Raut <rshantanu73@gmail.com>
2023-01-19 00:09:20 +0530

    Added my project

---
+++ wii.py
@@ -0,0 +1,13 @@
+# This file gives you headlines of wiki
+
+import bs4
+import requests
+import lxml
+
+
+res=requests.get('https://en.wikipedia.org/wiki/Machine_learning')
+soup=bs4.BeautifulSoup(res.text, 'lxml')
+# for i in soup.select('.mw-headline'):
+# 	print(i.text)
+for i in soup.select('.toctext'):
+	print(i.text)
